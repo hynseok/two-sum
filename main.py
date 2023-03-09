e52c632530bab412fe5de356191e3d5ca95d2c5e
hynseok <wefwef12e@naver.com>
2023-03-09 20:01:44 +0900

    two-sum

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     result = []
     for i in range(len(nums)):
         for j in range(len(nums)) :
-            if i==j: continue
-            if nums[i]+nums[j]==target: 
+            if i-1==j-1: continue
+            if nums[i-1]+nums[j-1]==target: 
                 result.insert(0, i-1)
                 result.insert(1, j-1)
                 return result
